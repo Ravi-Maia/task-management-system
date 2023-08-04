4ae51f43f3c1b27e4010bef61e19fe5a9cff9baa
2bXorNot <64798261+Ravi-Maia@users.noreply.github.com>
2023-08-04 12:08:16 -0300

    create retrieveUserInfo function to fetch User object in localStorage; create loadUserInfo that takes the object's id as a parameter to fetch the desired data; add a onload event to tms_member html body element that runs retriveUserInfo.

--- js/tmsLogin.js
+++ js/tmsLogin.js
@@ -22,16 +22,57 @@ function userLogIn() {
     .then(response => response.json())
     .then(response => {
         localStorage.setItem("tmsLogged", JSON.stringify(response))
-        console.log(response)
-        if(response.password === 'abc') {
+        console.log(response.password)
+        if(response.password === "abc2" || "def2") {
             window.location = "tms_member.html"
-        } else if(response.password === "yyz" + 1) {
+        } else if(response.password == 'yyz1') {
             window.location = "tms_pm.html"
         }
         
     })
     .catch(err=>{alert("Loadding error")})
 }
+
+function retrieveUserInfo() {
+    let info = localStorage.getItem("tmsLogged");
+    console.log(info);
+    let parseInfo = JSON.parse(info);
+    console.log(parseInfo.userName)
+    loadUserInfo(parseInfo.id);
+}
+
+function loadUserInfo(id) {
+
+    let url = "http://localhost:8080/personalContact/emp_id/" + id;
+    console.log(url);
+    fetch(url)
+    .then(response => response.json())
+    .then(response => {
+        console.log(response)
+        console.log(response.personalContact)
+    })
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
